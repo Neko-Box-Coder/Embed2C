8c569b22c75d9a6f7acb0a28d7f4a86c09a247cd
Neko Box Coder <nekoboxcoder@gmail.com>
2023-05-04 22:13:41 +0100

    Added size for embedded content

--- embed.c
+++ embed.c
@@ -5,25 +5,37 @@ int main(int argc, char *argv[])
 {
     FILE *fp;
     int i, j, ch;
+
+    printf("#include <stdint.h>\n");
+    printf("#include <stddef.h>\n");
+    
+    printf("%s", "\n");
+
+    printf("%s", "//Extern Declaration:\n");
+    for(i = 1; i < argc; i+=2) 
+    {
+        if(i + 1 >= argc)
+        {
+            printf("ERROR: Missing argument %d", i + 1);
+            exit(EXIT_FAILURE);
+        }
+        
+        printf("//extern const uint8_t %s[];\n", argv[i + 1]);
+        printf("//extern const size_t %s_size;\n", argv[i + 1]);
+    }
+    
+    printf("%s", "\n");
+    printf("%s", "//Embedded contents:\n");
+    
     for(i = 1; i < argc; i+=2) 
     {
         if((fp = fopen(argv[i], "rb")) == NULL)
         {
-            printf("Failed to read: %s", argv[i]);
-            exit(i);
+            printf("ERROR: Failed to read: %s", argv[i]);
+            exit(EXIT_FAILURE);
         } 
         else 
         {
-            if(i == 1)
-            {
-                printf("#include <stdint.h>\n");
-            }
-            
-            if(i + 1 >= argc)
-            {
-                exit(i+1);
-            }
-            
             printf("const uint8_t %s[] = {", argv[i + 1]);
             
             for (j = 0; (ch = fgetc(fp)) != EOF; j++) 
@@ -34,9 +46,14 @@ int main(int argc, char *argv[])
                 }
                 printf("%#04x, ", ch);
             }
-            // Append zero byte at the end, to make text files appear in memory
-            // as nul-terminated strings.
-            printf("%s", "0x00\n};\n");
+            
+            //// Append zero byte at the end, to make text files appear in memory
+            //// as nul-terminated strings.
+            //printf("%s", "0x00\n};\n");
+            printf("%s", "\n};\n");
+            
+            printf("const size_t %s_size = sizeof(%s);\n", argv[i + 1], argv[i + 1]);
+            
             fclose(fp);
         }
     }    
